1cffa3d8f84f82e74a034141894901f8c1ca9702
Simon Melchertsen <warcraft.simon@gmail.com>
2019-08-29 10:08:16 +0200

    Initial commit

---
+++ Console4CoolKids/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console4CoolKids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Core.Entities/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class Pet
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime SoldDate { get; set; }
+        public string Color { get; set; }
+        public string PreviousOwner { get; set; }
+        public double Price { get; set; }
+
+    }
+}
